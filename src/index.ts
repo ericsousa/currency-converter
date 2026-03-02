9ffa948fc8f4ac13b580b4120c121deee9ff8f28
Eric Sousa <ericsousa@gmail.com>
2026-03-01 21:48:43 -0300

    welcome message

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+let exchangeRate:number = 5.13
+
+console.log(`
+=======================================
+   CURRENCY CONVERTER - BRL / USD
+=======================================
+Este programa converte valores entre Real (BRL) e Dólar (USD).
+Cotação do dia: 1.00 USD = ${exchangeRate} BRL
+`);
