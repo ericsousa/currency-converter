5ae0b84fc28234160847363ea54b8f05d4869cc4
Eric Sousa <ericsousa@gmail.com>
2026-03-01 22:09:40 -0300

    BRL to USD conversion

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
+import promptSync from "prompt-sync";
 
-let exchangeRate:number = 5.13
+const prompt = promptSync();
+let exchangeRate:number = 5.00
 
 console.log(`
-=======================================
+=====================================s==
    CURRENCY CONVERTER - BRL / USD
 =======================================
 Este programa converte valores entre Real (BRL) e Dólar (USD).
-Cotação do dia: 1.00 USD = ${exchangeRate} BRL
+Cotação: 1.00 USD = ${exchangeRate} BRL
+
 `);
+
+console.log("Menu: ");
+console.log("1. BRL para USB");
+console.log("0. Sair");
+
+const option = prompt("Escolhar uma opção: ");
+
+switch (option) {
+    case '1':
+        const valorBrl = Number(prompt("Entre o valor em BRL: "));
+        console.log(`Resultado: ${(valorBrl / exchangeRate).toFixed(2)} USD`);
+        break;
+    case '0':
+        console.log("Saindo...");
+        break;
+    default:
+        console.log("Opção inválida.");
+}
+
